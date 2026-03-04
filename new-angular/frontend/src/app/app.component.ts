f37af47086ac7fa68f09f70535b8f0f3e4a948f5
maulik sarena <sarenamaulik65@gmail.com>
2026-03-04 14:05:56 +0530

    chnage the donor side UI and responsive created

--- new-angular/frontend/src/app/app.component.ts
+++ new-angular/frontend/src/app/app.component.ts
@@ -9,7 +9,9 @@ import { FooterComponent } from './components/footer/footer.component';
   imports: [RouterOutlet, NavbarComponent, FooterComponent],
   template: `
     <app-navbar></app-navbar>
-    <router-outlet></router-outlet>
+    <main class="app-main">
+      <router-outlet></router-outlet>
+    </main>
     <app-footer></app-footer>
   `,
   styles: []
